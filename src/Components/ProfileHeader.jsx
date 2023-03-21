db94fcd33746fce429ced7a59537ac20045e7771
Bryant Hong <bhong4059@gmail.com>
2023-03-20 22:44:15 -0700

    practicing making components

---
+++ src/Components/ProfileHeader.jsx
@@ -0,0 +1,16 @@
+export default function ProfileHeader(){
+    const frequentAgent = "https://img.redbull.com/images/c_crop,x_991,y_0,h_2160,w_1620/c_fill,w_400,h_540/q_auto:low,f_auto/redbullcom/2021/2/16/bm7cdtb6xdhcibbweehq/valorant-omen"; 
+    const profileName = "JoeyWarCrimes"; 
+    const playerRank = "https://i.imgflip.com/6eer1g.png"; 
+    return(
+        <div class="home-header"> 
+            <img>{frequentAgent}</img>
+            <div class="profile-name">
+                <h1>{profileName}</h1>
+            </div>
+            <div class="rank-icon">
+               <img>{playerRank}</img>     
+            </div>
+        </div>
+    ); 
+}
---
+++ src/Views/HomePage.jsx
@@ -0,0 +1,13 @@
+import ProfileHeader from "../Components/ProfileHeader";
+
+export default function HomePage() {
+  
+    return (
+        <div>
+            <ProfileHeader/> 
+        </div>
+      
+    );
+  }
+  
+  
